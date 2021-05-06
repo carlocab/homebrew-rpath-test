e3e4bd8ab18c283dabeaf4a00563603ff3fe70f3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-06 18:12:48 +0100

    swig: test `RPATH` relocation`

---
+++ Formula/swig@4.0.2.rb
@@ -0,0 +1,64 @@
+class SwigAT402 < Formula
+  desc "Generate scripting interfaces to C/C++ code"
+  homepage "http://www.swig.org/"
+  url "https://downloads.sourceforge.net/project/swig/swig/swig-4.0.2/swig-4.0.2.tar.gz"
+  sha256 "d53be9730d8d58a16bf0cbd1f8ac0c0c3e1090573168bfa151b01eb47fa906fc"
+  license "GPL-3.0-or-later"
+
+  head do
+    url "https://github.com/swig/swig.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  depends_on "pcre"
+
+  uses_from_macos "ruby" => :test
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      int add(int x, int y)
+      {
+        return x + y;
+      }
+    EOS
+    (testpath/"test.i").write <<~EOS
+      %module test
+      %inline %{
+      extern int add(int x, int y);
+      %}
+    EOS
+    (testpath/"run.rb").write <<~EOS
+      require "./test"
+      puts Test.add(1, 1)
+    EOS
+    system "#{bin}/swig", "-ruby", "test.i"
+    on_macos do
+      system ENV.cc, "-c", "test.c"
+      system ENV.cc, "-c", "test_wrap.c", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Ruby.framework/Headers/"
+      system ENV.cc, "-bundle", "-undefined", "dynamic_lookup", "test.o", "test_wrap.o", "-o", "test.bundle"
+    end
+    on_linux do
+      ruby = Formula["ruby"]
+      args = Utils.safe_popen_read(
+        ruby.opt_bin/"ruby", "-e", "'puts RbConfig::CONFIG[\"LIBRUBYARG\"]'"
+      ).chomp
+      system ENV.cc, "-c", "-fPIC", "test.c"
+      system ENV.cc, "-c", "-fPIC", "test_wrap.c",
+             "-I#{ruby.opt_include}/ruby-#{ruby.version.major_minor}.0",
+             "-I#{ruby.opt_include}/ruby-#{ruby.version.major_minor}.0/x86_64-linux/"
+      system ENV.cc, "-shared", "test.o", "test_wrap.o", "-o", "test.so",
+             *args.delete("'").split
+    end
+    assert_equal "2", shell_output("ruby run.rb").strip
+  end
+end
