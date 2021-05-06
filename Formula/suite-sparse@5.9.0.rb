0cbc2ed8f763651b0158519e758d231474cd5cc2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-06 18:20:44 +0100

    suite-sparse: test `RPATH` relocation

---
+++ Formula/suite-sparse@5.9.0.rb
@@ -0,0 +1,53 @@
+class SuiteSparseAT590 < Formula
+  desc "Suite of Sparse Matrix Software"
+  homepage "https://people.engr.tamu.edu/davis/suitesparse.html"
+  url "https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v5.9.0.tar.gz"
+  sha256 "7bdd4811f1cf0767c5fdb5e435817fdadee50b0acdb598f4882ae7b8291a7f24"
+  license all_of: [
+    "BSD-3-Clause",
+    "LGPL-2.1-or-later",
+    "GPL-2.0-or-later",
+    "Apache-2.0",
+    "GPL-3.0-only",
+    any_of: ["LGPL-3.0-or-later", "GPL-2.0-or-later"],
+  ]
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "metis"
+  depends_on "openblas"
+  depends_on "tbb"
+
+  uses_from_macos "m4"
+
+  def install
+    mkdir "GraphBLAS/build" do
+      system "cmake", "..", *std_cmake_args
+    end
+
+    args = [
+      "INSTALL=#{prefix}",
+      "BLAS=-L#{Formula["openblas"].opt_lib} -lopenblas",
+      "LAPACK=$(BLAS)",
+      "MY_METIS_LIB=-L#{Formula["metis"].opt_lib} -lmetis",
+      "MY_METIS_INC=#{Formula["metis"].opt_include}",
+    ]
+    system "make", "library", *args
+    system "make", "install", *args
+    lib.install Dir["**/*.a"]
+    pkgshare.install "KLU/Demo/klu_simple.c"
+  end
+
+  test do
+    system ENV.cc, "-o", "test", pkgshare/"klu_simple.c", "-I#{include}",
+           "-L#{lib}", "-lsuitesparseconfig", "-lklu"
+    assert_predicate testpath/"test", :exist?
+    assert_match "x [0] = 1", shell_output("./test")
+  end
+end
