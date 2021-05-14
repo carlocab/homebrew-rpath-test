2d5748bee8cb6ec6d3e36f60bebd8adde9bee6bf
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-14 07:10:54 +0100

    pdnsrec: test `RPATH` relocation

---
+++ Formula/pdnsrec@4.5.1.rb
@@ -0,0 +1,39 @@
+class PdnsrecAT451 < Formula
+  desc "Non-authoritative/recursing DNS server"
+  homepage "https://www.powerdns.com/recursor.html"
+  url "https://downloads.powerdns.com/releases/pdns-recursor-4.5.1.tar.bz2"
+  sha256 "3721a1d0e438a683735f518db1e91da6ace1b90fbfdb9c588adabdf164114e79"
+  license "GPL-2.0-only"
+
+  livecheck do
+    url "https://downloads.powerdns.com/releases/"
+    regex(/href=.*?pdns-recursor[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "lua"
+  depends_on "openssl@1.1"
+
+  def install
+    ENV.cxx11
+
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}/powerdns
+      --disable-silent-rules
+      --with-boost=#{Formula["boost"].opt_prefix}
+      --with-libcrypto=#{Formula["openssl@1.1"].opt_prefix}
+      --with-lua
+      --without-net-snmp
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    output = shell_output("#{sbin}/pdns_recursor --version 2>&1")
+    assert_match "PowerDNS Recursor #{version}", output
+  end
+end
